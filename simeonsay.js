2846c226650d83fa39693cb0de1b23ccd1217f18
manoj724 <mkumarc46@gmail.com>
2025-03-05 22:00:50 +0530

    Update simeonsay.js

--- simeonsay.js
+++ simeonsay.js
@@ -1,36 +1,3 @@
-// console.log("hello")
-// let gameseq=[];
-// let userseq=[];
-// let btns=["yellow","green","purple","red"];
-// let started=false;
-// let level=0;
-// let h2=document.querySelector("h2");
-// document.addEventListener("keypress",function(){
-//     if(started==false){
-//         console.log("game is started");
-//         started=true;
-//         levelup();
-//     }
-// });
-// function btnflash(btn){
-//     btn.classList.add("flash");
-//     setTimeout(function(){
-//         btn.classList.remove("flash");
-// },250)
-
-// }
-// function levelup(){
-//     level++;
-//     h2.innerText=`level ${level}`;
-//     let randomIdx=Math.floor(Math.random()*3);
-//     let randomcolor=btns[randomIdx];
-//     let randombtns=document.querySelector(`.${randomcolor}`);
-//     console.log(randomcolor);
-//     console.log(randomIdx);
-//     console.log(randombtns);
-//     btnflash(randombtns);
-// }
-
 
 let gameSeq = [];
 let userSeq = [];
